80244f0ea78e0df335f8b82902599f58c5e7a148
ogb <ongjjbrandon@gmail.com>
2023-09-15 18:31:18 +0800

    shirt size works

--- actions/actions.py
+++ actions/actions.py
@@ -11,20 +11,22 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-import datetime as dt 
+import datetime as dt
 
 
-class ActionHelloWorld(Action):
-
+class ActionSayShirtSize(Action):
     def name(self) -> Text:
-        return "action_show_time"
+        return "action_say_shirt_size"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        # dispatcher.utter_message(text=f"(dt.datetime.now().strftime('%H:%M:%S'))")
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
-
-
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        shirt_size = tracker.get_slot("shirt_size")
+        if not shirt_size:
+            dispatcher.utter_message(text="I don't know your shirt size.")
+        else:
+            dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
         return []
